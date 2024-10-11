c07cc7da8598e2b44ccd051eb53826b5072189a3
Alin Silcock <at.silcock@gmail.com>
2024-10-11 14:01:41 -0600

    finished lab

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import request, session
+from flask import request, session, make_response, jsonify
 from flask_restful import Resource
 from sqlalchemy.exc import IntegrityError
 
@@ -8,19 +8,117 @@ from config import app, db, api
 from models import User, Recipe
 
 class Signup(Resource):
-    pass
+    def post(self):
+        data = request.get_json()
+        
+        username = data.get('username')
+        password = data.get('password')
+
+        # Validate that both username and password are present
+        if not data.get('username') or not data.get('password'):
+            response = {"error": "Username and password are required"}
+            return make_response(jsonify(response), 422)
+
+        user = User.query.filter_by(username=data.get('username')).first()
+        if not user: 
+            user = User(
+                username=data.get('username'),
+                password_hash=data.get('password'),
+                image_url=data.get('image_url', ''),  # Default to empty string if not provided
+                bio=data.get('bio', '')  # Default to empty string if not provided
+            )
+
+            try:
+                db.session.add(user)
+                db.session.commit()
+
+                session['user_id'] = user.id
+
+                return make_response(jsonify(user.to_dict()), 201)
+            except IntegrityError:
+                db.session.rollback()
+                return make_response(jsonify({"error": "Username already exists"}), 422)
+
+        else:
+            response = {"error": "Username already exists"}
+            return make_response(jsonify(response), 422)
+
+                
 
 class CheckSession(Resource):
-    pass
+    def get(self):
+        user = session['user_id']
+        if not user:
+            response = {"message":"Not logged in"}
+            return make_response(jsonify(response), 401)
+        else:
+            user = User.query.filter_by(id = user).first()
+            return make_response(user.to_dict(), 200)
+
 
 class Login(Resource):
-    pass
+    def post(self):
+        data = request.get_json()
+        username = data.get('username')
+        password = data.get('password')
+        
+        if not username or not password:
+            response = {"error" : "username and password are requried"}
+            return make_response(jsonify(response, 422))
+        
+        user = User.query.filter_by(username = username).first()
+        
+        if user and user.check_password(password):
+            session['user_id'] = user.id 
+            return make_response(jsonify(user.to_dict()), 200)
+        else:
+            response = {"message":"incorrect password"}
+            return make_response(jsonify(response), 401)
 
 class Logout(Resource):
-    pass
+    def delete(self):
+        session.pop("user_id", None)
+        response = {}
+        return make_response(jsonify(response), 401)
 
 class RecipeIndex(Resource):
-    pass
+    def get(self):
+        if not session.get('user_id'):
+            return make_response(jsonify({"error":"Unauthorized"}), 401)
+        
+        recipies = [recipie.to_dict() for recipie in Recipe.query.all()]
+        return make_response(jsonify(recipies), 200)
+    
+    def post(self):
+        data = request.get_json()
+
+        title = data.get('title')
+        instructions = data.get('instructions')
+        minutes_to_complete = data.get("minutes_to_complete")
+
+        if not title or not instructions or not minutes_to_complete:
+            return make_response(jsonify({"message" :"Missing requried fields"}), 422)
+        
+        if len(instructions) <50:
+            return make_response(jsonify({"error": "Instructions must be longer than 50 charachters"}), 422)
+
+
+        user = User.query.filter_by(id = session.get("user_id")).first()
+        if user:
+            new_recipie = Recipe(
+                title = data.get('title'),
+                instructions = data.get('instructions'),
+                minutes_to_complete = data.get("minutes_to_complete"),
+                user_id = user.id
+            )
+            db.session.add(new_recipie)
+            db.session.commit()
+
+            return make_response(new_recipie.to_dict(), 201)
+        else:
+            return make_response({"message": "Unauthorized"}, 422)
+
+
 
 api.add_resource(Signup, '/signup', endpoint='signup')
 api.add_resource(CheckSession, '/check_session', endpoint='check_session')
--- server/models.py
+++ server/models.py
@@ -7,9 +7,51 @@ from config import db, bcrypt
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
 
-    pass
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique = True, nullable = False)
+    _password_hash = db.Column(db.String)
+    image_url = db.Column(db.String)
+    bio = db.Column(db.String)
 
+    recipes = db.relationship("Recipe", back_populates="user")
+
+    serialize_rules = ("-recipes.user",)
+
+
+    @property
+    def password_hash(self):
+        raise AttributeError ("Access is not allowed")
+    
+    @password_hash.setter
+    def password_hash(self, password):
+        self._password_hash = bcrypt.generate_password_hash(password)  
+
+    def check_password(self, password):
+       return bcrypt.check_password_hash(self._password_hash, password)
+    
+    def authenticate(self, password):
+        return self.check_password(password)
+
+    
+    
 class Recipe(db.Model, SerializerMixin):
     __tablename__ = 'recipes'
     
-    pass
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String, nullable = False)
+    instructions = db.Column(db.String, nullable = False)
+    minutes_to_complete = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user = db.relationship("User", back_populates="recipes")
+
+    serialize_rules = ("-user.recipies",)
+
+    @validates('instructions')
+    def validate_instructions(self, key,instructions):
+        if len(instructions) >= 50:
+            return instructions 
+        else: 
+            raise ValueError("length must be longer than 50")
+
+
+
